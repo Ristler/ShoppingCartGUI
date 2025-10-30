486b16614ddc1196bce5bc57e1b51d1b7eebaa2b
Daniel <100704382+Ristler@users.noreply.github.com>
2025-10-30 15:00:45 +0200

    Update window title to "Daniel Juntunen"

--- src/main/java/org/example/otp2gui/Main.java
+++ src/main/java/org/example/otp2gui/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 650, 450);
-        stage.setTitle("Shopping");
+        stage.setTitle("Daniel Juntunen");
         stage.setScene(scene);
         stage.show();
     }
